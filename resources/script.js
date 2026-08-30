736cb99820f1391caa768482ed4fbe2c0b72a106
WebMav <294071804+Webmav@users.noreply.github.com>
2026-08-30 17:13:34 +0530

    Declared numb1,numb2 and operator and made a operate function to operate a calc on number( step 2&3 )

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,9 @@
+//2
+let numb1;
+let numb2;
+let operator;
 
+//1
 function add(num1,num2){
     return num1+num2;
 }
@@ -10,4 +15,20 @@ function multiply(num1,num2){
 }
 function divide(num1,num2){
     return num1/num2;
+}
+
+//3
+function operate(numb1,operator,numb2){
+    if(operator == "+"){
+        return add(numb1,numb2);
+    }
+    else if(operator == "-"){
+        return subtract(numb1,numb2);
+    }
+    else if(operator == "*"){
+        return multiply(numb1,numb2);
+    }
+    else if(operator == "/"){
+        return divide(numb1,numb2);
+    }
 }
