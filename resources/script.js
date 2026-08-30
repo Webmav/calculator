bf306330fbd46688ddf76dc9483f009c95c20112
WebMav <294071804+Webmav@users.noreply.github.com>
2026-08-30 17:55:05 +0530

    Added a button event listener to update numb1 (or operator)

--- resources/script.js
+++ resources/script.js
@@ -32,3 +32,34 @@ function operate(numb1,operator,numb2){
         return divide(numb1,numb2);
     }
 }
+
+const button = document.querySelector("button")
+
+button.addEventListener('click',function(event){
+    if(event.target.tagName === "BUTTON"){
+        let buttonId = event.target.id;
+
+        switch(buttonId){
+            case "0":numb1=0;break;
+            case "1":numb1=1;break;
+            case "2":numb1=2;break;
+            case "3":numb1=3;break;
+            case "4":numb1=4;break;
+            case "5":numb1=5;break;
+            case "6":numb1=6;break;
+            case "7":numb1=7;break;
+            case "8":numb1=8;break;
+            case "9":numb1=9;break;
+
+            case "+":operator="+";break;
+            case "-":operator="-";break;
+            case "x":operator="*";break;
+            case "/":operator="/";break;
+
+            case "AC":numb1=0;break;
+            case "C":numb1=1;break;
+        }
+    console.log(numb1);
+    console.log(operator);
+    }
+})
