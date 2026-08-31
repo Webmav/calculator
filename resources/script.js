e047fed5deb7acaf6a552bf26a0ff2a82b10a89a
WebMav <294071804+Webmav@users.noreply.github.com>
2026-08-31 09:35:15 +0530

    Made calculator to perform on single digit numbers(6)

--- resources/script.js
+++ resources/script.js
@@ -2,6 +2,7 @@
 let numb1;
 let numb2;
 let operator;
+let last;
 
 //1
 function add(num1,num2){
@@ -36,6 +37,8 @@ function operate(numb1,operator,numb2){
 const buttons = document.querySelectorAll("button")
 
 //Now
+/*
+
 buttons.forEach((button)=>{
     button.addEventListener('click',function(event){
     if(event.target.tagName === "BUTTON"){
@@ -57,14 +60,133 @@ buttons.forEach((button)=>{
             case "-":operator="-";break;
             case "x":operator="*";break;
             case "/":operator="/";break;
-
-            case "AC":numb1=0;break;
-            case "=":numb1=1;break;
+            case "AC":numb1=0,numb2=0,operator="",last=0;break
+            case "=":last = operate(numb1,operator,numb2);break;
         }
-        log.textContent = `${numb1} ${operator} ${numb2} `
+        log.textContent = `${numb1}`
+
+        switch(buttonId){
+            case "+":operator="+";break;
+            case "-":operator="-";break;
+            case "x":operator="*";break;
+            case "/":operator="/";break;
+
+            case "AC":numb1=0,numb2=0,operator="",last=0;break
+            case "=":last = operate(numb1,operator,numb2);break;
+        }
+        log.textContent = `${numb1} ${operator} `
+
+        switch(buttonId){
+            case "0":numb2=0;break;
+            case "1":numb2=1;break;
+            case "2":numb2=2;break;
+            case "3":numb2=3;break;
+            case "4":numb2=4;break;
+            case "5":numb2=5;break;
+            case "6":numb2=6;break;
+            case "7":numb2=7;break;
+            case "8":numb2=8;break;
+            case "9":numb2=9;break;
+
+            case "+":operator="+";break;
+            case "-":operator="-";break;
+            case "x":operator="*";break;
+            case "/":operator="/";break;
+
+            case "AC":numb1=0,numb2=0,operator="",last=0;break
+            case "=":last = operate(numb1,operator,numb2);break;
+        }
+        log.textContent = `${numb1} ${operator} ${numb2} = ${last} `
+
     console.log(numb1);
     console.log(operator);
+    console.log(numb2);
+    console.log(last);
     }
 })})
 
+*/
+
 const log = document.querySelector("#log");
+
+let totalClicks = 0;
+buttons.forEach((button)=>{
+    button.addEventListener('click',function(event){
+        totalClicks++;
+        if(event.target.tagName==="BUTTON"){
+        let buttonId = event.target.id;
+
+            let turn = totalClicks%4;
+            if(buttonId==="AC"){
+                switch(buttonId){
+                    case "AC":numb1=0,numb2=0,operator="",last=0,targetClicks = 0;break;
+                }
+            }
+            if(turn===1){
+                switch(buttonId){
+                case "0":numb1=0;break;
+                case "1":numb1=1;break;
+                case "2":numb1=2;break;
+                case "3":numb1=3;break;
+                case "4":numb1=4;break;
+                case "5":numb1=5;break;
+                case "6":numb1=6;break;
+                case "7":numb1=7;break;
+                case "8":numb1=8;break;
+                case "9":numb1=9;break;
+
+                case "+":operator="+";break;
+                case "-":operator="-";break;
+                case "x":operator="*";break;
+                case "/":operator="/";break;
+
+                case "=":last = operate(numb1,operator,numb2);break;
+                }
+                log.textContent = `${numb1}`
+            }
+            else if(turn===2){
+                switch(buttonId){
+                    case "+":operator="+";break;
+                    case "-":operator="-";break;
+                    case "x":operator="*";break;
+                    case "/":operator="/";break;
+
+                    case "=":last = operate(numb1,operator,numb2);break;
+                }
+                log.textContent = `${numb1} ${operator} `
+            }
+            else if(turn===3){
+                switch(buttonId){
+                    case "0":numb2=0;break;
+                    case "1":numb2=1;break;
+                    case "2":numb2=2;break;
+                    case "3":numb2=3;break;
+                    case "4":numb2=4;break;
+                    case "5":numb2=5;break;
+                    case "6":numb2=6;break;
+                    case "7":numb2=7;break;
+                    case "8":numb2=8;break;
+                    case "9":numb2=9;break;
+
+                    case "+":operator="+";break;
+                    case "-":operator="-";break;
+                    case "x":operator="*";break;
+                    case "/":operator="/";break;
+
+                    case "=":last = operate(numb1,operator,numb2);break;
+                }
+                log.textContent = `${numb1} ${operator} ${numb2} `
+            }
+            else if(turn===0){
+                switch(buttonId){
+                    case "=":last = operate(numb1,operator,numb2);break;
+                }
+                log.textContent = `${numb1} ${operator} ${numb2} = ${last} `
+            }
+            if(totalClicks>=4){
+                totalClicks = 0;
+            }
+
+        }
+    })
+})
