633d499e9c34f1fe2a1bb9ef137961aa708079b7
webmav Extensa Ubuntu <294071804+Webmav@users.noreply.github.com>
2026-09-01 17:34:16 +0530

    Made sure that my calculator only runs an operation when supplied with two numbers and an operator by the user. Example: I enter a number (2), followed by an operator button (+) and press the operator button (+) a second consecutive time. My calculator would not evaluate this as (2 + 2) and would not display the result (4). If consecutive operator buttons are pressed, my calculator would not run any evaluations, it would only take the last operator entered to be used for the next operation.

--- resources/script.js
+++ resources/script.js
@@ -156,14 +156,13 @@ To see what this looks like in action, feel free to input the equation we just e
 //Pressing = before entering all of the numbers or an operator could cause problems!
 //Pressing “clear” should wipe out any existing data. Make sure the user is really starting fresh after 
 // ...pressing “clear”
-
 //Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator!
-
 //Make sure that your calculator only runs an operation when supplied with two numbers and an operator by the 
 // ...user. Example: you enter a number (2), followed by an operator button (+). You press the operator button (+)
 // ...a second consecutive time. Your calculator should not evaluate this as (2 + 2) and should not display 
 // ...the result (4). If consecutive operator buttons are pressed, your calculator should not run any 
 // ...evaluations, it should only take the last operator entered to be used for the next operation.
+
 //When a result is displayed, pressing a new digit should clear the result and start a new calculation 
 // ...instead of appending the digit to the existing result. Check whether this is the case on your calculator!
 
